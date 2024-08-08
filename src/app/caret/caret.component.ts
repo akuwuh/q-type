37a9c49ad88c312601202ade401f4e3a0e296c80
Isaac <isaacnguyen03@gmail.com>
2024-08-08 17:23:34 -0400

    Caret Tweakin Yo

--- src/app/caret/caret.component.ts
+++ src/app/caret/caret.component.ts
@@ -1,17 +1,19 @@
-import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-caret',
   standalone: true,
   imports: [],
   template: `
-    <div #caret id="caret" class="full-width default" [class.pace]="isActive" tabindex="-1"></div>
+    <div #caret id="caret" class="default" [class.pace]="isActive" tabindex="-1"></div>
   `,
   styleUrl: './caret.component.scss'
 })
-export class CaretComponent {
+export class CaretComponent implements OnInit {
 
-	
+	ngOnInit(): void {
+      this.updatePosition(6,6);
+  }
 
 	@Input() isActive: boolean = true;
 
