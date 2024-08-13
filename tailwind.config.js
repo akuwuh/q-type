6c1a1484bab13d77e44d20335d46084cad6d6360
akuwuh <isaacnguyen03@gmail.com>
2024-08-13 18:08:08 -0400

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,ts}', './projects/**/*.{html,ts}'],
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-  important: true,
-  mode: 'jit',
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader',
-        ],
-      },
-    ],
-  },
- }
+}
